45040333aefe21d440fc2587a3e4ecfa7a6ece6b
vagg <vagg.karanikolos@gmail.com>
2024-07-21 18:16:48 +0300

    fixing relation

--- database/migrations/2023_10_20_102809_create_shops_table.php
+++ database/migrations/2023_10_20_102809_create_shops_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
-            $table->integer('shopcategory_id');
+            $table->foreignId('shopcategory_id')->constrained();
             $table->longText('description');
             $table->string('open_hours');
             $table->string('city');
